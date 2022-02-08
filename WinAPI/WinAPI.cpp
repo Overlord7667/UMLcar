efb05cae46dee91ca3b5d1a3de01eeb828489488
Overlord7667 <aspiaro@gmail.com>
2022-02-08 21:54:19 +0300

    Godd

--- WinAPI/WinAPI.cpp
+++ WinAPI/WinAPI.cpp
@@ -1,6 +1,10 @@
 #include <Windows.h>
 #include"resource.h"
 //#define MESAGE_BOX
+#define MY_BTN 10 11
+
+CONST CHAR invite[] = "Введите логин";
+CONST CHAR invite2[] = "Введите пароль";
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -19,15 +23,69 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	//hwnd-окно
+	//uMsg-сщщбщение
+	//wParam, lParam- параметры сообщения
 	switch (uMsg)
 	{
-	case WM_INITDIALOG:break;
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		//SendMessage(GetDlgItem(hwnd, IDC_EDIT1 0, 0);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)invite);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)invite2);
+	}
+	break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
+		case IDC_BTN_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)buffer);
+		}
+		break;
 		case IDOK:MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
+		case IDC_EDIT1:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_GETTEXT, SIZE, (LPARAM)buffer);
+			switch (HIWORD(wParam))
+			{
+			case EN_SETFOCUS:
+			{
+				if (strcmp(buffer, invite) == 0)
+					SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)"");
+				break;
+			}
+			case EN_KILLFOCUS:
+			{
+				CONST INT SIZE = 256;
+				CHAR buffer[SIZE]{};
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_GETTEXT, SIZE, (LPARAM)buffer);
+				if (strcmp(buffer, "") == 0)SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)invite);
+				//SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)"");
+				break;
+			}
+			}
+		}
+		break;
+		case IDC_EDIT2:
+			switch (HIWORD(wParam))
+			{
+			case EN_SETFOCUS:
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)"");
+				//SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)"");
+				break;
+			}
+		break;
 		}
 	}
 		break;
